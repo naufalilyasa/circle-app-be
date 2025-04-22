cf7f618224d4cb21f5d23b186b3ffe5614d77533
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-04-22 11:54:44 +0700

    feat: remove confirm password joi schema

--- src/validation/auth.ts
+++ src/validation/auth.ts
@@ -1,16 +1,15 @@
 import Joi from "joi";
 
 const registerSchema = Joi.object({
-  name: Joi.string().required(),
-  username: Joi.string().required(),
-  email: Joi.string().email().required(),
+  name: Joi.string().trim().required(),
+  email: Joi.string().email().trim().required(),
+  username: Joi.string().lowercase().trim().required(),
   password: Joi.string().min(6).required(),
-  confirmPassword: Joi.string().min(6).required(),
 });
 
 const loginSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export { registerSchema, loginSchema };
