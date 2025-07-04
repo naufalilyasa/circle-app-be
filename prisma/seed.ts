9e6be61628cc545aba9e90c0ac8bdd6244be98cc
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-07-04 17:42:47 +0700

    feat: add cloudinary dependency and implement seeding for likes and replies in the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,49 +1,128 @@
+import { Prisma } from "@prisma/client";
 import { PrismaClient } from "../src/generated/client";
+import { randomUUID } from "crypto";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const deleteTweets = await prisma.thread.deleteMany({});
-  const deleteUsers = await prisma.user.deleteMany({});
+  // const deleteTweets = await prisma.tweet.deleteMany({});
+  // const deleteUsers = await prisma.user.deleteMany({});
 
-  // Buat beberapa user
-  // const user1 = await prisma.user.create({
-  //   data: {
-  //     name: "Alice",
-  //     username: "alice123",
-  //     email: "alice@example.com",
-  //     password: "hashed_password_1", // ganti dengan password hasil hashing di production
-  //   },
-  // });
+  const naufalId = await prisma.user.findUnique({
+    where: {
+      id: "cd857921-2177-4002-82f5-dbd0055259ed",
+    },
+    omit: {
+      password: true,
+      passwordResetAt: true,
+      passwordResetToken: true,
+      verificationCode: true,
+      provider: true,
+    },
+  });
 
-  // const user2 = await prisma.user.create({
-  //   data: {
-  //     name: "Bob",
-  //     username: "bobby",
-  //     email: "bob@example.com",
-  //     password: "hashed_password_2",
-  //   },
-  // });
+  const koikoiId = await prisma.user.findUnique({
+    where: {
+      id: "87775920-4de2-423a-9f1a-79ad417be18d",
+    },
+    omit: {
+      password: true,
+      passwordResetAt: true,
+      passwordResetToken: true,
+      verificationCode: true,
+      provider: true,
+    },
+  });
 
-  // // Buat tweet dari masing-masing user
+  const tweet1Id = randomUUID();
+  const tweet2Id = randomUUID();
+  // const reply1Id = randomUUID();
+  const reply2Id = randomUUID();
+
+  if (!naufalId) {
+    throw new Error("tidak ditemukan user");
+  }
+
+  if (!koikoiId) {
+    throw new Error("tidak ditemukan user");
+  }
+
   // await prisma.tweet.createMany({
   //   data: [
   //     {
-  //       content: "Hello world! Ini tweet pertama dari Alice.",
-  //       authorId: user1.id,
+  //       id: tweet1Id,
+  //       content:
+  //         "Akhir-akhir ini mulai belajar TypeScript, ternyata enak juga ya dibanding JavaScript biasa.",
+  //       authorId: naufalId.id,
   //     },
   //     {
-  //       content: "Bob di sini, mencicipi dunia tweet.",
-  //       authorId: user2.id,
-  //     },
-  //     {
-  //       content: "Tweet kedua dari Alice. Hari ini menyenangkan!",
-  //       authorId: user1.id,
+  //       id: tweet2Id,
+  //       content:
+  //         "Mulai nerapin rutinitas pagi: bangun jam 5, journaling, dan olahraga ringan. Hidup terasa lebih terarah.",
+  //       authorId: naufalId.id,
   //     },
   //   ],
   // });
 
-  console.log("✅ Seed berhasil dibuat.");
+  // await prisma.reply.createMany({
+  //   data: [
+  //     {
+  //       content:
+  //         "Setuju banget! Type safety-nya bantu ngurangin bug kecil yang sering luput.",
+  //       tweetId: tweet1Id,
+  //       authorId: koikoiId.id,
+  //     },
+  //     {
+  //       content:
+  //         "Keren! Gue juga lagi coba habit tracker, ternyata pengaruhnya ke produktivitas lumayan besar.",
+  //       tweetId: tweet2Id,
+  //       authorId: koikoiId.id,
+  //     },
+  //     {
+  //       content:
+  //         "Iya bener! Apalagi pas bikin function gede, auto-complete-nya lebih jelas.",
+  //       tweetId: "b14ea479-9a92-478d-bec1-797e38daba6e",
+  //       authorId: naufalId.id,
+  //     },
+  //     {
+  //       content:
+  //         "Dan enaknya lagi, kolaborasi tim jadi lebih gampang karena kontraknya udah jelas dari tipe datanya.",
+  //       tweetId: "b14ea479-9a92-478d-bec1-797e38daba6e",
+  //       authorId: naufalId.id,
+  //     },
+  //   ],
+  // });
+
+  await prisma.like.createMany({
+    data: [
+      {
+        userId: naufalId.id,
+        tweetId: null,
+        replyId: "ed958a22-9131-4eb9-9ef3-1f8a6836259c",
+      },
+      {
+        userId: koikoiId.id,
+        tweetId: "b14ea479-9a92-478d-bec1-797e38daba6e",
+        replyId: null,
+      },
+      {
+        userId: koikoiId.id,
+        tweetId: null,
+        replyId: "69ea2bd0-6c48-46bf-b9b5-ec19c94b814d",
+      },
+    ],
+    skipDuplicates: true,
+  });
+  await prisma.reply.update({
+    where: {
+      id: "76f5fc36-1184-4a8f-90a1-149ad6752b8c",
+    },
+    data: {
+      createdAt: new Date(),
+    },
+  });
+
+  console.log("✅ Seed selesai: users, tweets, replies, likes.");
 }
 
 main()
