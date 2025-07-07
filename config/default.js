589df7f30772c72dc37b9781cc46c2c76035140f
Naufal Ilyasa <naufal.ilyasa7@gmail.com>
2025-07-07 14:54:51 +0700

    fix: config.js

--- config/default.js
+++ config/default.js
@@ -1,20 +1,22 @@
-export const port = parseInt(process.env.PORT || "3000");
-export const accessTokenPrivateKey = process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY;
-export const accessTokenPublicKey = process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY;
-export const refreshTokenPrivateKey = process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY;
-export const refreshTokenPublicKey = process.env.JWT_REFRESH_TOKEN_PUBLIC_KEY;
-export const redisCacheExpiresIn = 1440;
-export const refreshTokenExpiresIn = 1440;
-export const accessTokenExpiresIn = 60;
-export const origin = "http://localhost:3000";
-export const smtp = {
-  host: process.env.EMAIL_HOST,
-  pass: process.env.EMAIL_PASS,
-  port: Number(process.env.EMAIL_PORT),
-  user: process.env.EMAIL_USER
-};
-export const gmail = {
-  googleAppPassword: process.env.GOOGLE_APP_PASSWORD,
-  googleSenderMail: process.env.GOOGLE_SENDER_MAIL,
-  googleUser: process.env.GOOGLE_USER
+module.exports = {
+  port: parseInt(process.env.PORT || "3000"),
+  accessTokenPrivateKey: process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY,
+  accessTokenPublicKey: process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY,
+  refreshTokenPrivateKey: process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY,
+  refreshTokenPublicKey: process.env.JWT_REFRESH_TOKEN_PUBLIC_KEY,
+  redisCacheExpiresIn: 1440,
+  refreshTokenExpiresIn: 1440,
+  accessTokenExpiresIn: 60,
+  origin: "http://localhost:3000",
+  smtp: {
+    host: process.env.EMAIL_HOST,
+    pass: process.env.EMAIL_PASS,
+    port: Number(process.env.EMAIL_PORT),
+    user: process.env.EMAIL_USER,
+  },
+  gmail: {
+    googleAppPassword: process.env.GOOGLE_APP_PASSWORD,
+    googleSenderMail: process.env.GOOGLE_SENDER_MAIL,
+    googleUser: process.env.GOOGLE_USER,
+  },
 };
